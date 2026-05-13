6087367bcc37f9f209d9c1e1774bade8f1fba0ab
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2026-05-13 17:49:24 +0300

    cardinal spline

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <deque>
 #include <windows.h>
 #include <cmath>
 #include <string>
@@ -53,6 +54,12 @@ int ellipseRadiusAX, ellipseRadiusAY;
 int ellipseRadiusBX, ellipseRadiusBY;
 int ellipsePointsClicked = 0;
 int currentEllipseAlgorithm = 0; // 0=Direct, 1=Polar, 2=Midpoint
+
+//Curves
+struct Vector2;
+int waitingForCurve = false;
+deque<Vector2> splinePoints;
+const float CARDINAL_TENSION = 0.5;
 
 // Cursor state variable
 int currentCursorType = 0; // 0=Arrow, 1=Hand, 2=Wait, 3=Cross, 4=IBeam, 5=No, 6=SizeNS, 7=SizeWE
@@ -830,12 +837,115 @@ void drawEllipseMidpoint(int centerX, int centerY, int radA, int radB)
 
 // ==================== Curves Menu Functions ====================
 
-void drawCardinalSplineCurve()
+struct Vector2
+{
+	double x,y;
+
+	Vector2(double a=0,double b=0)
+	{
+		x=a; y=b;
+	}
+
+	Vector2 operator-(Vector2& v){
+		return Vector2(x - v.x, y - v.y);
+	}
+	
+	Vector2 operator*(float val){
+		return Vector2(x * val, y * val);
+	}
+};
+class Vector4
+{
+	double v[4];
+public:
+	Vector4(double a=0,double b=0,double c=0,double d=0)
+	{
+		v[0]=a; v[1]=b; v[2]=c; v[3]=d;
+	}
+	Vector4(double a[])
+	{
+		memcpy(v,a,4*sizeof(double));
+	}
+	double& operator[](int i)
+	{
+		return v[i];
+	}
+};
+class Matrix4
+{
+	Vector4 M[4];
+public:
+	Matrix4(double A[])
+	{
+		memcpy(M,A,16*sizeof(double));
+	}
+	Vector4& operator[](int i)
+	{
+		return M[i];
+	}
+};
+
+Vector4 operator*(Matrix4 M,Vector4& b) // right multiplication of M by b
+{
+	Vector4 res;
+	
+	for(int i=0;i<4;i++)
+	for(int j=0;j<4;j++)
+		res[i]+=M[i][j]*b[j];
+
+	return res;
+}
+
+double DotProduct(Vector4& a,Vector4& b) //multiplying a raw vector by a column vector
+{
+	return a[0]*b[0]+a[1]*b[1]+a[2]*b[2]+a[3]*b[3];
+}
+
+Vector4 GetHermiteCoeff(double x0,double s0,double x1,double s1)
+{
+	static double H[16]={2,1,-2,1,-3,-2,3,-1,0,1,0,0,1,0,0,0};
+	static Matrix4 basis(H);
+	Vector4 v(x0,s0,x1,s1);
+	return basis*v;
+}
+
+void DrawHermiteCurve (Vector2 P0,Vector2 T0,Vector2 P1,Vector2 T1 ,int
+numpoints)
+{
+	Vector4 xcoeff=GetHermiteCoeff(P0.x,T0.x,P1.x,T1.x);
+	Vector4 ycoeff=GetHermiteCoeff(P0.y,T0.y,P1.y,T1.y);
+	if(numpoints<2)return;
+	
+
+
+    HPEN hPen = CreatePen(PS_SOLID, 1, currentColor);
+    HPEN hOldPen = (HPEN)SelectObject(hdcBuffer, hPen);
+
+
+	double dt=1.0/(numpoints-1);
+	
+	for(double t=0;t<=1;t+=dt)
+	{
+		Vector4 vt;
+		vt[3]=1;
+		for(int i=2;i>=0;i--)vt[i]=vt[i+1]*t;
+		int x=round(DotProduct(xcoeff,vt));
+		int y=round(DotProduct(ycoeff,vt));
+		if(t==0)MoveToEx(hdcBuffer,x,y,NULL);
+		else LineTo(hdcBuffer,x,y);
+	}
+
+	SelectObject(hdcBuffer, hOldPen);
+    DeleteObject(hPen);
+}
+
+void drawCardinalSplineCurve(Vector2 P0,Vector2 T0,Vector2 P1,Vector2 T1)
 {
     cout << "Drawing Cardinal Spline Curve..." << endl;
 
+	DrawHermiteCurve(P0,T0,P1,T1,100);
     // TODO: Implement Cardinal Spline Curve algorithm
-    cout << "Cardinal Spline Curve drawn!" << endl;
+    cout << "Cardinal Spline Curve drawn!" << endl; 
     drawnShapes.push_back("CURVE_CARDINAL_SPLINE");
     InvalidateRect(hMainWindow, NULL, FALSE);
 }
@@ -1536,7 +1646,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         // Curves Menu
         else if (wmId == IDM_CURVE_SPLINE)
         {
-            drawCardinalSplineCurve();
+			waitingForCurve = true;
+            // drawCardinalSplineCurve();
         }
         // Filling Menu
         else if (wmId == IDM_FILL_CIRCLE_LINES)
@@ -1689,7 +1800,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
             InvalidateRect(hwnd, NULL, FALSE);
         }
-        break;
+        if(waitingForCurve){
+			waitingForCurve = false;
+
+			Vector2 t0;
+			Vector2 t1;
+			
+			int n = splinePoints.size();
+			t0 = (splinePoints[n - 1] - splinePoints[n - 3]) * (1 - CARDINAL_TENSION);
+			t1 = (splinePoints[n - 1] - splinePoints[n - 2]) * (1 - CARDINAL_TENSION);
+
+			drawCardinalSplineCurve(splinePoints[n - 2],t0,splinePoints[n - 1], t1);
+			splinePoints.clear();
+		}
+		break;
     }
 
     case WM_LBUTTONDOWN:
@@ -1852,7 +1976,34 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             }
             return 0;
         }
-        break;
+        if (waitingForCurve){
+			
+            int mouseX = GET_X_LPARAM(lParam);
+            int mouseY = GET_Y_LPARAM(lParam);
+			Vector2 p;
+			p.x = mouseX;
+			p.y = mouseY;
+
+			splinePoints.push_back(p);
+			cout << p.x << " " << p.y << "\n";
+			int n = splinePoints.size();
+			
+			Vector2 t0;
+			Vector2 t1;
+
+			if(n == 3){
+				t0 = (splinePoints[1] - splinePoints[0]) * (1 - CARDINAL_TENSION);
+				t1 = (splinePoints[2] - splinePoints[0]) * (1 - CARDINAL_TENSION);
+				drawCardinalSplineCurve(splinePoints[0],t0,splinePoints[1],t1);
+			}
+			else if(n > 3){
+				t0 = (splinePoints[n - 2] - splinePoints[n - 4]) * (1 - CARDINAL_TENSION);
+				t1 = (splinePoints[n - 1] - splinePoints[n - 3]) * (1 - CARDINAL_TENSION);
+				drawCardinalSplineCurve(splinePoints[n - 3],t0,splinePoints[n - 2],t1);
+				cout << "DRAWN\n";
+			}
+		}
+		break;
     }
 
     case WM_PAINT:
